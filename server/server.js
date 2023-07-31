4edc735305ca1b0e925bca1101019928a57c8589
Anthony Apodaca <adapodaca92@gmail.com>
2023-07-31 10:43:42 -0700

    Cleaned up code a bit.

--- server/server.js
+++ server/server.js
@@ -3,16 +3,11 @@ const app = express();
 const mongoose = require('mongoose');
 const passport = require('passport');
 const logger = require('morgan');
-// const flash = require('express-flash');
 const connectDB = require('./config/database');
-
 
 require('dotenv').config({path: './config/.env'});
 
 const PORT = process.env.PORT || 8500;
-
-// passport config
-// require('./config/passport')(passport);
 
 connectDB();
 
@@ -21,12 +16,9 @@ app.use(express.json());
 app.use('/public', express.static(__dirname + '/public'));
 app.use(logger('dev'))
 
-// app.use(flash());
-
 // ROUTERS
 
 const homeRoutes = require('./routes/homeRoutes');
-// const userRoutes = 
 
 app.use('/', homeRoutes);
 // app.use('/user', userRoutes);
